a09e282f6d105185a307e0c07889373c0ecdc602
RedSnail <Oleg.Demianchenko@gmail.com>
2019-06-13 21:37:40 +0300

    added some data not working

---
+++ app/src/main/java/msu/ug/MainActivity.kt
@@ -0,0 +1,20 @@
+package msu.ug
+
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        setContentView(R.layout.activity_main)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_frame, ChoiseFragment(this))
+        transaction.commit()
+    }
+}
