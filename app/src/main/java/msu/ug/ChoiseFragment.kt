ca7d05da510d5df14775b66529bf536738f1522d
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2019-06-14 21:09:04 +0300

    Initial data display (#3)
    * renamed data files
    
    * the root statement should be displayed (i haven't tested yet)

--- app/src/main/java/msu/ug/ChoiseFragment.kt
+++ app/src/main/java/msu/ug/ChoiseFragment.kt
@@ -3,21 +3,44 @@ package msu.ug
 import android.content.Context
 import android.widget.SimpleAdapter
 import androidx.fragment.app.ListFragment
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
 
-class ChoiseFragment : ListFragment() {
+class ChoiseFragment(private val actContext: Context) : ListFragment() {
+    private val storage = Storage(actContext)
+
     override fun onStart() {
         super.onStart()
-        val data = ArrayList<Map<String, Any>>(2)
-        val map1 = HashMap<String, Any>()
-        map1["text"] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        val map2 = HashMap<String, Any>()
-        map2["text"] = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-        data.add(map1)
-        data.add(map2)
 
+        val arr = JSONArray(getPlainText(storage.currentChoise.toString()))
+
+        val displayData = ArrayList<Map<String, Any>>(2)
+
+        (0 until arr.length()).forEach {i ->
+            val map = HashMap<String, Any>()
+            map["text"] = arr.getJSONObject(i).getString("text")
+            displayData.add(map)
+        }
+      
         val from = arrayOf("text")
         val to = intArrayOf(android.R.id.text1)
-        val listAdapter = SimpleAdapter(context, data, android.R.layout.simple_list_item_1, from, to)
+        val listAdapter = SimpleAdapter(context, displayData, android.R.layout.simple_list_item_1, from, to)
         setListAdapter(listAdapter)
+    }
+
+    private fun getPlainText(statementFile : String) : String {
+        val statementStream = actContext.assets.open(statementFile)
+        val br = BufferedReader(InputStreamReader(statementStream))
+        var line : String? = br.readLine()
+        val sb = StringBuilder()
+
+        while (line != null) {
+            sb.append(line)
+            line = br.readLine()
+        }
+
+        return sb.toString()
     }
 }
