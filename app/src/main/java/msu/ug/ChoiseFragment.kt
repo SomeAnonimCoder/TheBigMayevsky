0b70cd01b731e5ea2d6efb86d851719be7cd13bf
RedSnail <Oleg.Demianchenko@gmail.com>
2019-06-15 23:50:48 +0300

    primitive surfing

--- app/src/main/java/msu/ug/ChoiseFragment.kt
+++ app/src/main/java/msu/ug/ChoiseFragment.kt
@@ -1,6 +1,8 @@
 package msu.ug
 
 import android.content.Context
+import android.view.View
+import android.widget.ListView
 import android.widget.SimpleAdapter
 import androidx.fragment.app.ListFragment
 import org.json.JSONArray
@@ -10,24 +12,27 @@ import java.lang.StringBuilder
 
 class ChoiseFragment(private val actContext: Context) : ListFragment() {
     private val storage = Storage(actContext)
+    private val toIndices = ArrayList<Int>()
 
     override fun onStart() {
         super.onStart()
 
-        val arr = JSONArray(getPlainText(storage.currentChoise.toString()))
-
-        val displayData = ArrayList<Map<String, Any>>(2)
-
-        (0 until arr.length()).forEach {i ->
-            val map = HashMap<String, Any>()
-            map["text"] = arr.getJSONObject(i).getString("text")
-            displayData.add(map)
-        }
-      
         val from = arrayOf("text")
         val to = intArrayOf(android.R.id.text1)
+        val displayData = ArrayList<Map<String, Any>>()
+        updateDisplayData(displayData)
         val listAdapter = SimpleAdapter(context, displayData, android.R.layout.simple_list_item_1, from, to)
         setListAdapter(listAdapter)
+        storage.addSwitchListener {
+            updateDisplayData(displayData)
+            listAdapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onListItemClick(l: ListView?, v: View?, position: Int, id: Long) {
+        super.onListItemClick(l, v, position, id)
+        storage.currentChoise = toIndices[position]
+
     }
 
     private fun getPlainText(statementFile : String) : String {
@@ -42,5 +47,18 @@ class ChoiseFragment(private val actContext: Context) : ListFragment() {
         }
 
         return sb.toString()
+    }
+
+    private fun updateDisplayData(displayData: ArrayList<Map<String, Any>>) {
+        val arr = JSONArray(getPlainText(storage.currentChoise.toString()))
+        toIndices.clear()
+        displayData.clear()
+        (0 until arr.length()).forEach {i ->
+            val map = HashMap<String, Any>()
+            val thesa = arr.getJSONObject(i)
+            map["text"] = thesa.getString("text")
+            toIndices.add(thesa.getInt("to"))
+            displayData.add(map)
+        }
     }
 }
--- app/src/main/java/msu/ug/Storage.kt
+++ app/src/main/java/msu/ug/Storage.kt
@@ -5,9 +5,20 @@ import android.content.SharedPreferences
 
 class Storage (context : Context) {
     private val sp : SharedPreferences = context.getSharedPreferences("MY_SP", Context.MODE_PRIVATE)
+    private val switchListeners : ArrayList<() -> Unit> = ArrayList()
+
+    fun addSwitchListener(listener : () -> Unit) {
+        switchListeners.add(listener)
+    }
+
     var currentChoise : Int
         set(value) {
-            sp.edit().putInt("current_choise", value).apply()
+            if (value > 0) {
+                sp.edit().putInt("current_choise", value).apply()
+                switchListeners.forEach {
+                    it.invoke()
+                }
+            }
         }
         get() {
             return sp.getInt("current_choise", 1)
