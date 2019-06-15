a092511f7498fe35b50c914a30fd47caf6ecc3bb
glubshev2001 <glubshev2001@mail.ru>
2019-06-15 23:54:55 +0300

    Style changed

--- app/src/main/java/msu/ug/AboutActivity.kt
+++ app/src/main/java/msu/ug/AboutActivity.kt
@@ -1,12 +1,23 @@
 package msu.ug
 
+import android.content.pm.ActivityInfo
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        val tv:TextView = findViewById(R.id.info_tv)
+        val sp = getString(R.string.info)
+        tv.setText(sp)
     }
+
 }
