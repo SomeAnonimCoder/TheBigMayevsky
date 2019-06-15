1762c232b443fd32526189ac79fd53c7bb5d8225
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2019-06-16 02:38:46 +0300

    Revert "Fix hardcoded text (#30)"
    This reverts commit 38fd82fadef6a0b71996b82bfab1fdedd98e691f.

--- app/src/main/java/msu/ug/AboutActivity.kt
+++ app/src/main/java/msu/ug/AboutActivity.kt
@@ -1,10 +1,13 @@
 package msu.ug
 
 import android.content.pm.ActivityInfo
-import android.os.Bundle
-import android.widget.TextView
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
-
+import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class AboutActivity : AppCompatActivity() {
 
@@ -14,8 +17,7 @@ class AboutActivity : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         val tv:TextView = findViewById(R.id.info_tv)
         val sp = getString(R.string.info)
-        tv.text = sp
-
+        tv.setText(sp)
     }
 
 }
